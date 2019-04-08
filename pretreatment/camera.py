73357f36737cc14f294638a1d8493e6d18767b34
jinxq <3197464389@qq.com>
2019-04-08 12:09:00 +0800

    do some experiments

---
+++ pretreatment/camera.py
@@ -0,0 +1,11 @@
+import cv2
+import time
+
+cap = cv2.VideoCapture(2)
+ret,frame = cap.read()
+
+t = time.time()
+t_str = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(t))
+
+cv2.imwrite("%s.png"%t_str,frame)
+cap.release()
